08ec908084aa320fcb045b97eca306d812b09ab6
Jinawong <jokjack26172@gmail.com>
2019-05-13 22:44:50 +0700

    update refact route

--- src/module/weapon/route.js
+++ src/module/weapon/route.js
@@ -1,30 +1,31 @@
-const controller = require('./controller')
+const controller = require("./controller");
 
-module.exports = (server) => {
-    server.route([
-        {
-            method: 'GET',
-            path:'/weapons',
-            handler: controller.getList
-        },
-        {
-            method: 'GET',
-            path:'/weapons/{id}',
-            handler: controller.getDetail
-        },
-        {
-            method: 'POST',
-            path:'/weapons',
-            handler: controller.create
-        },
-        {
-            method: 'PUT',
-            path:'/weapons/{id}',
-            handler: controller.update
-        },{
-            method: 'DELETE',
-            path:'/weapons/{id}',
-            handler: controller.remove
-        }
-    ])
-}
+module.exports = server => {
+  server.route([
+    {
+      method: "GET",
+      path: "/weapons",
+      handler: controller.getList
+    },
+    {
+      method: "GET",
+      path: "/weapons/{id}",
+      handler: controller.getDetail
+    },
+    {
+      method: "POST",
+      path: "/weapons",
+      handler: controller.create
+    },
+    {
+      method: "PUT",
+      path: "/weapons/{id}",
+      handler: controller.update
+    },
+    {
+      method: "DELETE",
+      path: "/weapons/{id}",
+      handler: controller.remove
+    }
+  ]);
+};
